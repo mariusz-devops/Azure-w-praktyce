0c5ded8153090227336ce85a44f6c960f4f5b835
Mariusz Dworniczak <Mariusz.Dworniczak@gmail.com>
2026-02-14 15:17:52 +0100

    Dodanie kodu08 - api.ts

---
+++ kody/Kod08 - api.ts/api.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+const api = axios.create({
+// Tutaj React pobiera adres z pliku .env, który właśnie stworzyłeś
+baseURL: import.meta.env.VITE_API_URL,
+});
+export default api;
